c6fce488920e060b6a38b9c00b07e9667fc5615d
Arendelle12 <gabriela.bartoszewska@student.put.poznan.pl>
2020-12-01 11:52:34 +0100

    Get input from user

--- client.py
+++ client.py
@@ -6,7 +6,13 @@ PORT = 1234
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello server')
+
+    print('Napisz cos')
+    string = input()
+    byt = bytes(string, 'utf-8')
+
+    #s.sendall(b'Hello server')
+    s.sendall(byt)
     data = s.recv(1024)
 
 print('RECEIVED', data.decode())
