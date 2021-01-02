a58e3fb18518c200c94ccc28c040dfeb7da9078f
Arendelle12 <gabriela.bartoszewska@student.put.poznan.pl>
2021-01-02 22:49:24 +0100

    Add window to get host and port

--- client.py
+++ client.py
@@ -2,60 +2,65 @@ from constants import *
 from mappers import *
 from board import Board
 from network import Network
+from inputWindow import InputWindow
 from time import sleep
 
-my_turn = False
 
-#FPS = 60
+def run_game(host, port):
+    my_turn = False
 
-#game window
-board_2d = []
-with Network() as network:
-    #narysowanie okna z plansza
-    pygame_board = Board()
-    pygame_board.draw_squares()
+    #FPS = 60
 
-    #clock
-    #clock = pygame.time.Clock()
-    #run
-    #run = True
+    #game window
+    board_2d = []
+    with Network(host, port) as network:
+        #narysowanie okna z plansza
+        pygame_board = Board()
+        pygame_board.draw_squares()
 
-    #while(True):
-        #odebranie informacji 
-    player_number = network.recv(1)
-    print(player_number)
-    if(player_number == "1"):
-        my_turn = True
+        #clock
+        #clock = pygame.time.Clock()
+        #run
+        #run = True
 
-    while(1):
-        #clock.tick(FPS)
-        rec_str = network.recv(64)
-
-        """
-        if(run == True):
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-
-        if(run == False):
-            pygame.quit() 
-        """      
-
-        if(rec_str == "Twoj ruch\x00"):
+        #while(True):
+            #odebranie informacji 
+        player_number = network.recv(1)
+        print(player_number)
+        if(player_number == "1"):
             my_turn = True
-            print("TRUE: ", rec_str)
-        else:
-            print("Otrzymano wiadomosc: ")
-            board_2d = convert_board(rec_str)
-            pygame_board.draw(board_2d)
 
-        if(my_turn == True):
-            pygame_board.show_text()
-            sleep(1)
-            pygame_board.draw(board_2d)
-            #wyslanie ruchu
-            start_field, end_field = pygame_board.get_moves()
-            move = move_to_string(start_field, end_field)
-            print(move)
-            network.sendall(move)
-            my_turn = False
+        while(1):
+            #clock.tick(FPS)
+            rec_str = network.recv(64)
+
+            """
+            if(run == True):
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        run = False
+
+            if(run == False):
+                pygame.quit() 
+            """      
+
+            if(rec_str == "Twoj ruch\x00"):
+                my_turn = True
+                print("TRUE: ", rec_str)
+            else:
+                print("Otrzymano wiadomosc: ")
+                board_2d = convert_board(rec_str)
+                pygame_board.draw(board_2d)
+
+            if(my_turn == True):
+                pygame_board.show_text()
+                sleep(1)
+                pygame_board.draw(board_2d)
+                #wyslanie ruchu
+                start_field, end_field = pygame_board.get_moves()
+                move = move_to_string(start_field, end_field)
+                print(move)
+                network.sendall(move)
+                my_turn = False
+
+input_window = InputWindow(callback=run_game)
---
+++ inputWindow.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+import re
+
+class InputWindow:
+    def __init__(self, callback):
+        self.callback = callback
+        self.root = tk.Tk()
+        
+        self.box = tk.Canvas(self.root, width = 400, height = 350)
+        self.box.pack()
+        self.entry1 = tk.Entry(self.root)
+        # self.entry1.insert(0, "127.0.0.1")
+        self.entry2 = tk.Entry(self.root)
+        # self.entry2.insert(0, "1234")
+        self.validationError = tk.StringVar()
+        self.window()
+
+    def get_input(self):
+        pattern = re.compile("^\d+\.\d+\.\d+\.\d+$")
+        host = self.entry1.get()
+        port = self.entry2.get()
+
+        if not bool(pattern.match(host)):
+            self.validationError.set(f'Host is not IPv4 {host}')
+        elif not port.isdigit():
+            self.validationError.set(f'Port is not an int {port}')
+        else:
+            self.root.destroy()
+            self.callback(host, int(port))
+
+    def window(self):
+        label1 = tk.Label(self.root, text = 'IP ADDRESS')
+        self.box.create_window(200, 50, window = label1)
+        
+        self.box.create_window(200, 100, window = self.entry1)
+
+        label2 = tk.Label(self.root, text = 'PORT')
+        self.box.create_window(200, 150, window = label2)
+
+        self.entry2 = tk.Entry(self.root)
+        self.box.create_window(200, 190, window = self.entry2)
+
+        button1 = tk.Button(text="OK", command = self.get_input, bg = 'blue', fg = 'white')
+        self.box.create_window(150, 250, window = button1)
+
+        label3 = tk.Label(self.root, textvariable = self.validationError)
+        self.box.create_window(200, 290, window = label3)
+
+        self.root.mainloop()
